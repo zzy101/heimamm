fb276683cf52be44d3f208012d28d42ce27c1150
zzy101 <1351286464@qq.com>
2019-12-13 14:04:58 +0800

    登陆页面 配置路径成功

--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,20 @@
 // 导入vue
 import Vue from 'vue'
+
 // 导入 vue-router
 import VueRouter from 'vue-router'
 //注册 vue-router
 Vue.use(VueRouter)
+
+import login from '../views/login/login.vue'
 // 实例化
 const router = new VueRouter({
     //这里就是路由的配制项
     routes: [
         {
-            path: '',
-            // component:  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
- }
+            path: '/login',
+            component: login  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+        },
     ]
 })
 
