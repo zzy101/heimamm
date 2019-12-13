abaf051b43424bb2ed1ec2be751514c728556c9a
zzy101 <1351286464@qq.com>
2019-12-13 11:31:42 +0800

    项目启动 配置element-ui 成功

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import App from './App.vue'
 // 导入路由
 import router from './router/router.js'
 
+// 导入 element-ui 和 element-ui的样式
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+//注册element-ui
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
