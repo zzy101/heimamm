71c067b8189b2721abb01e16169d7197d23fbb58
zzy101 <1351286464@qq.com>
2019-12-13 17:02:07 +0800

    登录页面 - 标题完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="login-container">
-      <!-- 登录表单 -->
-    <div class="login-from"></div>
+    <!-- 登录表单 -->
+    <div class="login-from">
+        <!-- 标题 -->
+      <div class="login-title">
+        <img src="../../assets/login-log.png" alt />
+        <span class="title">黑马面面</span>
+        <i></i>
+        <span class="sub-title">用户登录</span>
+      </div>
+    </div>
     <!-- 右边的图片 -->
     <div class="login-img">
       <img src="../../assets/login_banner_ele.png" alt />
@@ -31,6 +39,33 @@ export default {};
     width: 478px;
     height: 550px;
     background-color: #f5f5f5;
+    padding: 44px;
+
+    /* 标题 */
+    .login-title {
+        display: flex;
+        align-items: center;
+        /* log */
+        img {
+            width: 22px;
+            height: 17px;
+            margin-right: 16px;
+        }
+        .title {
+            font-size: 24px;
+            margin-right: 14px;
+        }
+        i {
+            width: 1px;
+            height: 28px;
+            background-color: #C7C7C7;
+            margin-right: 12px;
+        }
+        .sub-title {
+            font-size: 22px;
+        }
+
+    }
   }
 }
 </style>
