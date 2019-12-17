1924319d6fcb257d8098caae38a84360a833dba6
zzy101 <1351286464@qq.com>
2019-12-17 17:39:31 +0800

    首页页面 - 通用布局完成

--- src/router/router.js
+++ src/router/router.js
@@ -6,7 +6,12 @@ import VueRouter from 'vue-router'
 //注册 vue-router
 Vue.use(VueRouter)
 
+// 导入登录页面 - login
 import login from '../views/login/login.vue'
+
+// 导入首页页面 - index
+import index from '../views/index/index.vue'
+
 // 实例化
 const router = new VueRouter({
     //这里就是路由的配制项
@@ -14,6 +19,10 @@ const router = new VueRouter({
         {
             path: '/login',
             component: login  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+        },
+        {
+            path: '/index',
+            component: index  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
         },
     ]
 })
---
+++ src/views/index/index.vue
@@ -0,0 +1,117 @@
+<template>
+  <el-container>
+    <el-header class="index-header">
+      <div class="header-left">
+        <i @click="isCollapse = !isCollapse" class="el-icon-s-operation"></i>
+        <img src="../../assets/login-log.png" alt />
+        <span>黑马面面</span>
+      </div>
+      <div class="header-right">
+        <img src="../../assets/login-log.png" alt />
+        <span class="username">
+          <i>李达</i>,你好
+        </span>
+        <el-button type="primary">退出</el-button>
+      </div>
+    </el-header>
+    <el-container>
+      <!-- 左边导航栏 -->
+      <el-aside width="auto">
+        <el-menu default-active="1" class="el-menu-vertical-demo" :collapse="isCollapse">
+          <!-- 数据概览 -->
+          <el-menu-item index="1">
+            <i class="el-icon-pie-chart"></i>
+            <span slot="title">数据概览</span>
+          </el-menu-item>
+          <!-- 用户列表 -->
+          <el-menu-item index="2">
+            <i class="el-icon-user"></i>
+            <span slot="title">用户列表</span>
+          </el-menu-item>
+          <!-- 题库列表 -->
+          <el-menu-item index="3">
+            <i class="el-icon-edit-outline"></i>
+            <span slot="title">题库列表</span>
+          </el-menu-item>
+          <!-- 企业列表 -->
+          <el-menu-item index="4">
+            <i class="el-icon-office-building"></i>
+            <span slot="title">企业列表</span>
+          </el-menu-item>
+          <!-- 学科列表 -->
+          <el-menu-item index="5">
+            <i class="el-icon-notebook-2"></i>
+            <span slot="title">学科列表</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <!-- 右边主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isCollapse: false
+    };
+  }
+};
+</script>
+
+<style lang="less">
+// 首页头部
+.index-header {
+  // background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  // 头部左边
+  .header-left {
+    display: flex;
+    align-content: center;
+    i {
+      font-size: 24px;
+      margin-right: 22px;
+    }
+    img {
+      width: 33px;
+      height: 28px;
+      margin-right: 11px;
+    }
+    span {
+      font-size: 22px;
+      color: #49a1ff;
+    }
+  }
+  // 头部右边
+  .header-right {
+    display: flex;
+    align-content: center;
+    img {
+      border-radius: 50%;
+      width: 43px;
+      margin-right: 9px;
+    }
+    .username {
+      font-size: 14px;
+      margin-right: 38px;
+      display: flex;
+      align-items: center;
+      i {
+        font-style: normal;
+      }
+    }
+  }
+}
+// 首页导航栏
+.el-aside {
+  // 首页左边
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+}
+</style>
