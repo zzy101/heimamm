4be203f92e56dc91d10172611ff3215e968d3ec9
zzy101 <1351286464@qq.com>
2019-12-20 20:34:37 +0800

    题库列表 - 基本布局(存档)

---
+++ src/views/index/question/components/questionAdd.vue
@@ -0,0 +1,90 @@
+<template>
+  <el-dialog title="收货地址" :visible.sync="$parent.addFormVisible" center>
+    <el-form :model="form">
+      <el-form-item label="学科" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="请选择活动学科">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="阶段" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="请选择活动阶段">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="企业" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="请选择活动企业">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="城市" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="请选择活动城市">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="题型" :label-width="formLabelWidth">
+        <el-radio v-model="form.radio" label="1">单选</el-radio>
+        <el-radio v-model="form.radio" label="2">多选</el-radio>
+        <el-radio v-model="form.radio" label="3">简答</el-radio>
+      </el-form-item>
+      <el-form-item label="难度" :label-width="formLabelWidth">
+        <el-radio v-model="form.radio" label="1">简单</el-radio>
+        <el-radio v-model="form.radio" label="2">一般</el-radio>
+        <el-radio v-model="form.radio" label="3">困难</el-radio>
+      </el-form-item>
+      <el-divider></el-divider>
+      <el-form-item label="试题标题" :label-width="formLabelWidth">
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="$parent.addFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="$parent.addFormVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+        radio: "1"
+      },
+      formLabelWidth: "80px"
+    };
+  }
+};
+</script>
+
+<style lang='less'>
+.el-dialog__wrapper {
+  .el-dialog {
+    width: 1430px;
+    // 头部标题颜色
+    .el-dialog__header {
+      background: linear-gradient(to right, #02c5fa, #1394fa);
+    }
+    .el-form {
+      padding-left: 300px;
+    //   文字靠左
+      .el-form-item__label{
+          text-align: left
+      }
+      .el-divider--horizontal {
+        width: 830px;
+      }
+    }
+  }
+}
+</style>
--- src/views/index/question/question.vue
+++ src/views/index/question/question.vue
@@ -60,69 +60,84 @@
         <el-form-item class="btn">
           <el-button type="primary">搜索</el-button>
           <el-button>清除</el-button>
-          <el-button icon="el-icon-plus" type="primary">新增企业</el-button>
+          <el-button @click="addFormVisible = true" icon="el-icon-plus" type="primary">新增题目</el-button>
         </el-form-item>
       </el-form>
     </el-card>
     <el-card class="box-card">
       <el-table :data="tableData" border style="width: 100%">
         <el-table-column prop="date" label="序号"></el-table-column>
-        <el-table-column prop="name" label="题目"></el-table-column>
-        <el-table-column prop="address" label="学科阶段"></el-table-column>
-        <el-table-column prop="address" label="题型"></el-table-column>
-        <el-table-column prop="address" label="企业"></el-table-column>
-        <el-table-column prop="address" label="创建者"></el-table-column>
-        <el-table-column prop="address" label="状态"></el-table-column>
+        <el-table-column prop="title" label="题目"></el-table-column>
+        <el-table-column prop="step" label="学科阶段"></el-table-column>
+        <el-table-column prop="type" label="题型"></el-table-column>
+        <el-table-column prop="enterprise" label="企业"></el-table-column>
+        <el-table-column prop="creater" label="创建者"></el-table-column>
+        <el-table-column prop="status" label="状态"></el-table-column>
         <el-table-column prop="address" label="访问量"></el-table-column>
         <el-table-column prop="address" label="操作"></el-table-column>
       </el-table>
     </el-card>
+    <!-- 新增题目 -->
+    <questionAdd></questionAdd>
   </div>
 </template>
 
 <script>
+// 导入 API 抽取层
+import { questionList } from "../../../API/question.js";
+// 导入 新增 组件
+import questionAdd from "./components/questionAdd.vue";
+
 export default {
+  components: {
+    questionAdd
+  },
   data() {
     return {
+      // 表单数据
       formInline: {
         user: "",
         region: "",
         date1: "",
         date2: ""
       },
+      // 新增对话框是否显示
+      addFormVisible: false,
+      // 表格数据
       tableData: []
     };
   },
   created() {
-    
-  },
+    questionList().then(res => {
+      window.console.log(res);
+    });
+  }
 };
 </script>
 
 <style lang="less">
 .question-container {
-
   .question-form {
     margin-bottom: 20px;
-  }
-  // input框样式
-  .el-form-item {
-    margin-left: 20px;
-    .el-form-item__content {
-      width: 150px;
+    // input框样式
+    .el-form-item {
+      margin-left: 20px;
+      .el-form-item__content {
+        width: 150px;
+      }
     }
-  }
-  // input框 标题样式
-  .el-form-item__label {
-    margin-right: 20px;
-  }
-  // 按钮样式
-  .btn .el-form-item__content {
-    width: 100%;
-  }
-  // 标题样式
-  .title .el-form-item__content {
-    width: 388px;
+    // input框 标题样式
+    .el-form-item__label {
+      margin-right: 20px;
+    }
+    // 按钮样式
+    .btn .el-form-item__content {
+      width: 100%;
+    }
+    // 标题样式
+    .title .el-form-item__content {
+      width: 388px;
+    }
   }
 }
 </style>
