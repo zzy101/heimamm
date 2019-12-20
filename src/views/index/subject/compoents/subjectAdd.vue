9028a965e7b348b1b43acb44ab2e3567d3f16c85
zzy101 <1351286464@qq.com>
2019-12-20 11:29:04 +0800

    学科列表 - 基本布局完成

---
+++ src/views/index/subject/compoents/subjectAdd.vue
@@ -0,0 +1,107 @@
+<template>
+  <!-- 新增用户 -->
+  <el-dialog class="subjectAdd" title="新增用户" :visible.sync="$parent.addFormVisible" center>
+    <el-form :model="addForm" ref="addFrom" :rules="addRulse">
+      <!-- 学科编号-->
+      <el-form-item label="学科编号" :label-width="formLabelWidth" prop="rid">
+        <el-input v-model="addForm.rid" autocomplete="off"></el-input>
+      </el-form-item>
+      <!-- 学科名称-->
+      <el-form-item label="学科名称" :label-width="formLabelWidth" prop="name">
+        <el-input v-model="addForm.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <!-- 学科简称-->
+      <el-form-item label="学科简称" :label-width="formLabelWidth" prop="short_name">
+        <el-input v-model="addForm.short_name" autocomplete="off"></el-input>
+      </el-form-item>
+      <!-- 学科简介-->
+      <el-form-item label="学科简介" :label-width="formLabelWidth" prop="intro">
+        <el-input v-model="addForm.intro" autocomplete="off"></el-input>
+      </el-form-item>
+      <!-- 学科备注-->
+      <el-form-item label="学科备注" :label-width="formLabelWidth" prop="rema">
+        <el-input v-model="addForm.rema" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <!-- 确认按钮 -->
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="$parent.addFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="submitForm">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+// 导入user 抽取层
+import { subjectAdd } from "../../../../API/subject";
+export default {
+  data() {
+    return {
+      // 新增列表
+      addForm: {
+        rid: "", //学科编号
+        name: "", //学科名称
+        short_name: "", //学科简称
+        intro: "", //学科简介
+        rema: "" //学科备注
+      },
+      //   新增列表规则
+      addRulse: {
+        //   学科编号
+        rid : [
+            {required: true, message: '请输入学科编号', trigger: 'blur'}
+        ],
+        // 学科名称
+        name : [
+            {required: true, message: '请输入学科名称', trigger: 'blur'}
+        ],
+        // 学科简称
+        short_name : [
+            {required: true, message: '请输入学科简称', trigger: 'blur'}
+        ],
+        // 学科简介
+        intro : [
+            {required: true, message: '请输入学科简介', trigger: 'blur'}
+        ],
+      },
+      formLabelWidth: "80px"
+    };
+  },
+  methods: {
+    //   提交表单验证
+    submitForm() {
+      this.$refs.addFrom.validate(valid => {
+        if (valid) {
+          subjectAdd({ ...this.addForm }).then(res => {
+            if (res.code === 200) {
+              this.$message.success("新增成功");
+            //   this.$parent.getList(); //重新刷新数据
+              this.$parent.addFormVisible = false; //关闭对话框
+            } else if (res.code === 201) {
+              this.$message.warning("用户名已存在");
+            }
+            // $parent.addFormVisible = false;
+          });
+        } else {
+          this.$message.warning("新增失败!!!");
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.subjectAdd {
+  .el-dialog {
+    width: 603px;
+    .el-dialog__header {
+      background: linear-gradient(to right, #01c5fa, #1395fa);
+      .el-dialog__title {
+        color: white;
+      }
+    }
+  }
+}
+</style>
--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -31,28 +31,48 @@
         </el-form-item>
         <!-- 新增按钮 -->
         <el-form-item>
-          <el-button type="primary" class="el-icon-plus">新增学科</el-button>
+          <el-button @click="addFormVisible = true" type="primary" class="el-icon-plus">新增学科</el-button>
         </el-form-item>
       </el-form>
     </el-card>
     <el-card class="box-card subject-table">
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="serial" label="序号"></el-table-column>
-        <el-table-column prop="number" label="学科编号"></el-table-column>
-        <el-table-column prop="subject" label="学科名称"></el-table-column>
-        <el-table-column prop="short" label="简称"></el-table-column>
-        <el-table-column prop="name" label="创建者"></el-table-column>
-        <el-table-column prop="date" label="创建日期"></el-table-column>
+        <el-table-column type="index" label="序号"></el-table-column>
+        <el-table-column prop="rid" label="学科编号"></el-table-column>
+        <el-table-column prop="name" label="学科名称"></el-table-column>
+        <el-table-column prop="short_name" label="简称"></el-table-column>
+        <el-table-column prop="username" label="创建者"></el-table-column>
+        <el-table-column prop="create_time" label="创建日期"></el-table-column>
         <el-table-column prop="status" label="状态"></el-table-column>
         <el-table-column prop="operation" label="操作"></el-table-column>
       </el-table>
     </el-card>
+    <!-- 分页 -->
+      <!-- @size-change="handleSizeChange"
+      @current-change="handleCurrentChange" -->
+    <el-pagination
+      background
+      :current-page="page"
+      :page-sizes="page_sizes"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    ></el-pagination>
+    <!-- 新增学科 -->
+    <subjectAdd></subjectAdd>
   </div>
 </template>
 
 <script>
+// 导入 API 接收 抽取层
+import { subjectList } from "../../../API/subject.js";
 
+// 导入新增组件
+import subjectAdd from "./compoents/subjectAdd";
 export default {
+  components: {
+    subjectAdd
+  },
   data() {
     return {
       // 表单数据
@@ -63,22 +83,30 @@ export default {
       },
       // 表格数据
       tableData: [
-        {
-          serial: "", //序号
-          number: "", //学科编号
-          subject: "", //学科名称
-          short: "", //简称
-          name: "", //创建者
-          date: "", //创建日期
-          status: "", //状态
-          operation: "" //操作
-        }
-      ]
+        // {
+        //   serial: "", //序号
+        //   number: "", //学科编号
+        //   subject: "", //学科名称
+        //   short: "", //简称
+        //   name: "", //创建者
+        //   date: "", //创建日期
+        //   status: "", //状态
+        //   operation: "" //操作
+        // }
+      ],
+      // 新增框是否显示
+      addFormVisible: false,
+      // 页容量
+      page_sizes:[100, 200, 300, 400],
+      page : 1,  //当前页
     };
   },
   methods: {},
   created() {
-    
+    subjectList().then(res => {
+      window.console.log(res)
+      this.tableData = res.data.items;
+    });
   }
 };
 </script>
@@ -90,6 +118,11 @@ export default {
     .width100 .el-form-item__content {
       width: 100px;
     }
+  }
+  // 分页样式
+  .el-pagination  {
+    margin-top: 30px;
+    text-align: center
   }
 }
 </style>
